8e6f6c0af9df56fe30941bcec5aef4c9564b3b7c
dainuojing <dainuojing@youzan.com>
2022-12-09 14:22:43 +0800

    add log

--- vendor/github.com/moby/buildkit/worker/result.go
+++ vendor/github.com/moby/buildkit/worker/result.go
@@ -2,6 +2,9 @@ package worker
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"time"
 
 	"github.com/moby/buildkit/cache"
 	cacheconfig "github.com/moby/buildkit/cache/config"
@@ -33,12 +36,25 @@ func (wr *WorkerRef) GetRemotes(ctx context.Context, createIfNeeded bool, refCfg
 	if w, ok := wr.Worker.(interface {
 		GetRemotes(context.Context, cache.ImmutableRef, bool, cacheconfig.RefConfig, bool, session.Group) ([]*solver.Remote, error)
 	}); ok {
-		return w.GetRemotes(ctx, wr.ImmutableRef, createIfNeeded, refCfg, all, g)
+		f, _ := os.Create("/tmp/buildtimecostinlinecachegetremote1")
+		defer f.Close()
+		t1 := time.Now()
+		r, e := w.GetRemotes(ctx, wr.ImmutableRef, createIfNeeded, refCfg, all, g)
+		t2 := time.Now()
+		f.WriteString(fmt.Sprintf("get remotes inner: %f", t2.Sub(t1).Seconds()))
+		return r, e
 	}
 	if wr.ImmutableRef == nil {
 		return nil, nil
 	}
-	return wr.ImmutableRef.GetRemotes(ctx, createIfNeeded, refCfg, all, g)
+
+	f, _ := os.Create("/tmp/buildtimecostinlinecachegetremote2")
+	defer f.Close()
+	t1 := time.Now()
+	r, e := wr.ImmutableRef.GetRemotes(ctx, createIfNeeded, refCfg, all, g)
+	t2 := time.Now()
+	f.WriteString(fmt.Sprintf("get remotes of immutable ref: %f", t2.Sub(t1).Seconds()))
+	return r, e
 }
 
 type workerRefResult struct {
